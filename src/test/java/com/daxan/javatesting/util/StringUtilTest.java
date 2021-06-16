724d10580492088cd4e8b60b6378e7b0a057472b
Luis Daniel AH <luisdaniel.a.h.0307@outlook.com>
2021-06-15 23:55:50 -0500

    Video 2-3 Test unitario con JUnit

--- src/test/java/com/daxan/javatesting/util/StringUtilTest.java
+++ src/test/java/com/daxan/javatesting/util/StringUtilTest.java
@@ -1,22 +1,17 @@
 package com.daxan.javatesting.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
-        //System.out.println(result);
-        assertEquals(StringUtil.repeat("hola", 3),"holaholahola" );
+    @Test
+    public void testRepeat() {
 
-
-        //System.out.println(result2);
-        assertEquals(StringUtil.repeat("hola", 1), "hola");
-
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
 
     }
 
-    private static void assertEquals(String actual, String expected) {
-        if(!actual.equals(expected)){
-            //System.out.println("ERROR");
-            throw new RuntimeException(actual +" no es igual que " + expected);
-        }
-    }
+
 
 }
