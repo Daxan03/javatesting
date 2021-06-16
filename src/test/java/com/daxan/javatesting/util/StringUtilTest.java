ec2ea17ca1bd92b7a3c8f37789f44a544be2e6c8
Luis Daniel AH <luisdaniel.a.h.0307@outlook.com>
2021-06-15 23:26:46 -0500

    Video 2-2 Test unitario

--- src/test/java/com/daxan/javatesting/util/StringUtilTest.java
+++ src/test/java/com/daxan/javatesting/util/StringUtilTest.java
@@ -2,19 +2,21 @@ package com.daxan.javatesting.util;
 
 public class StringUtilTest {
     public static void main(String[] args) {
-        String result = StringUtil.repeat("hola", 3);
         //System.out.println(result);
-        if(!result.equals("holaholahola")){
-            System.out.println("ERROR");
-        }
+        assertEquals(StringUtil.repeat("hola", 3),"holaholahola" );
 
-        String result2 = StringUtil.repeat("hola", 1);
+
         //System.out.println(result2);
-        if(!result2.equals("hola")) {
-            System.out.println("ERROR");
+        assertEquals(StringUtil.repeat("hola", 1), "hola");
+
+
+    }
+
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected)){
+            //System.out.println("ERROR");
+            throw new RuntimeException(actual +" no es igual que " + expected);
         }
-
-
     }
 
 }
