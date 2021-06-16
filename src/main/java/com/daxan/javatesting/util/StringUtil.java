d6a24cd47c45c41969be791a1d1f48bab1c37248
Luis Daniel AH <luisdaniel.a.h.0307@outlook.com>
2021-06-16 00:24:29 -0500

    Video 2-4 organización del test con JUnit

--- src/main/java/com/daxan/javatesting/util/StringUtil.java
+++ src/main/java/com/daxan/javatesting/util/StringUtil.java
@@ -2,7 +2,11 @@ package com.daxan.javatesting.util;
 
 public class StringUtil {
     public static String repeat(String str, int times) {
-        //return null;
+
+        if (times < 0){
+            throw new IllegalArgumentException("No esta permitido que el numero de repeticiones sea negativo");
+        }
+
         String result = "";
         for (int i = 0; i < times; i++) {
             result += str;
--- src/test/java/com/daxan/javatesting/util/StringUtilTest.java
+++ src/test/java/com/daxan/javatesting/util/StringUtilTest.java
@@ -5,11 +5,22 @@ import org.junit.Test;
 
 public class StringUtilTest {
     @Test
-    public void testRepeat() {
-
+    public void repeat_string_once() {
         Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
-        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+    }
 
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("hola", -1);
     }
 
 
